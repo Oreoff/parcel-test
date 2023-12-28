bce200f7bfc4c41176465c7d6adbe2f1b91220d0
Oreoff <144335863+Oreoff@users.noreply.github.com>
2023-12-28 17:39:19 +0000

    Deploying to gh-pages from @ Oreoff/parcel-test@60cdd3e714ff1a31a63a9c90cc0a101f45c01fd5 🚀

---
+++ index.ed4d533c.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".header-burger").click((function(e){$(".header-burger,.header-menu").toggleClass("active"),$("body").toggleClass("lock")}))}));
+//# sourceMappingURL=index.ed4d533c.js.map
