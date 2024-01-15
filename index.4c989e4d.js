d1812bb1cf4a5755f2760e054076bc5074512a21
Oreoff <144335863+Oreoff@users.noreply.github.com>
2024-01-15 09:03:44 +0000

    Deploying to gh-pages from @ Oreoff/parcel-test@978b70f695d5f0f91663ebf8bdc708ef54379379 🚀

---
+++ index.4c989e4d.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".header-burger").click((function(e){$(".header-burger,.header-menu").toggleClass("active"),$("body").toggleClass("lock"),$(".header-socials-list").toggleClass("visible"),$(".header-address").toggleClass("visible")}))}));
+//# sourceMappingURL=index.4c989e4d.js.map
--- index.ed4d533c.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".header-burger").click((function(e){$(".header-burger,.header-menu").toggleClass("active"),$("body").toggleClass("lock")}))}));
-//# sourceMappingURL=index.ed4d533c.js.map
